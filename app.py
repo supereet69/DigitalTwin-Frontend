20cc3dec6f7db10df24e8cd117f91a2c950c4b9b
Supreet Naik <naiksupreet6@gmail.com>
2026-01-20 18:26:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="Leaching Digital Twin Platform",
-    layout="wide"
-)
+st.set_page_config(page_title="Leaching Digital Twin", layout="wide")
+
+st.markdown("""
+<style>
+  .block-container { padding: 0rem 0rem 0rem 0rem !important; }
+  header { visibility: hidden; }
+</style>
+""", unsafe_allow_html=True)
 
 with open("index.html", "r", encoding="utf-8") as f:
     html = f.read()
 
-st.components.v1.html(html, height=950, scrolling=True)
+st.components.v1.html(html, height=1200, scrolling=True)
